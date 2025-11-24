b1e55b070c31405304a2e8ccc56462511a320fcb
anrysys <andriiakishyn@gmail.com>
2025-11-24 16:14:43 +0200

    style: format import statements in user_search_page.dart
    Add a blank line before the import statements for better readability.

--- client/lib/features/messaging/presentation/pages/user_search_page.dart
+++ client/lib/features/messaging/presentation/pages/user_search_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../../../core/di/injection.dart';
 import '../../../../core/storage/secure_storage.dart';
 import '../../../../generated/auth.pb.dart' show UserSearchResult;
