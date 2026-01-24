f56799485b0252c04138ce355cf38a99ed897784
anrysys <andriiakishyn@gmail.com>
2026-01-24 12:34:35 +0200

    feat: add share_plus and url_launcher plugins
    Integrate share_plus and url_launcher plugins for enhanced
    sharing and URL launching capabilities across platforms.
    This improves user experience by enabling sharing features
    and external link handling.

--- client-mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client-mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,8 +12,10 @@ import flutter_local_notifications
 import flutter_secure_storage_macos
 import flutter_webrtc
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
+import url_launcher_macos
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -24,7 +26,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
